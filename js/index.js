9cbda9ab0f1a1c695febf813505f218bd0147d8d
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-11-03 21:02:28 -0300

    Atualizado index e destalhe dos carros

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const wrapper = document.querySelector(".carrossel-wrapper");
+const indicadores = document.querySelector(".carrossel-indicadores");
+const logos = document.querySelectorAll(".logo-item");
+
+let index = 0;
+const visiveis = 5; // Quantos logos aparecem por vez
+const total = logos.length;
+
+// quantidade real de slides
+const totalSlides = Math.ceil((total - visiveis) / visiveis) + 1;
+
+// cria indicadores de forma correta
+for (let i = 0; i < totalSlides; i++) {
+  const span = document.createElement("span");
+  if (i === 0) span.classList.add("ativo");
+  indicadores.appendChild(span);
+}
+
+const dots = indicadores.querySelectorAll("span");
+
+function mudarSlide() {
+  index++;
+
+  // impede de ir para slide vazio
+  if (index >= totalSlides) index = 0;
+
+  const deslocamento = index * (160 + 30) * visiveis;
+  const limiteMax = (total * (160 + 30)) - ((160 + 30) * visiveis);
+
+  // se deslocar além do total, trava no último grupo
+  const deslocFinal = Math.min(deslocamento, limiteMax);
+
+  wrapper.style.transform = `translateX(-${deslocFinal}px)`;
+
+  dots.forEach(dot => dot.classList.remove("ativo"));
+  dots[index].classList.add("ativo");
+}
+
+setInterval(mudarSlide, 3000);
